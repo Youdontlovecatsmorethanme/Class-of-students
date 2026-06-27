8cb41b661f5fad38f1b35228507a734d2e991789
Youdontlovecatsmorethanme <budacosmina069@gmail.com>
2026-06-27 07:04:46 +0300

    Enable adding multiple students to CSV
    Added functionality to input multiple student details and write them to a CSV file.

--- ClassOfStudents.py
+++ ClassOfStudents.py
@@ -9,13 +9,20 @@ class StudentInfo:
     def to_list(self):
         return [self.name, self.age, self.gpa, self.city]
 
-print("Enter student details")
-Name = input("Enter name:")
-Age = input("Enter age:")
-Gpa = input("Enter gpa:")
-City = input("Enter city:")
-new_student = StudentInfo(Name, Age, Gpa, City)
+count = int(input("Enter how many students you want to add: "))
+
 
 with open('student_data.csv', 'w', newline='') as csvfile:
     writer =csv.writer(csvfile)
-    writer.writerow(new_student.to_list())
+
+    writer.writerow(['Name', 'Age', 'Gpa', 'City'])
+
+    for i in range(count):
+        print("Enter student details")
+        Name = input("Enter name:")
+        Age = input("Enter age:")
+        Gpa = input("Enter gpa:")
+        City = input("Enter city:")
+
+        new_student = StudentInfo(Name, Age, Gpa, City)
+        writer.writerow(new_student.to_list())
